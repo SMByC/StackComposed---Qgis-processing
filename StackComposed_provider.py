1964bc7dd5706da3162a3527fc84116584be346d
XavierCLL <xavier.corredor.llano@gmail.com>
2019-09-14 22:16:02 -0700

    Add the plugin icon

--- StackComposed_provider.py
+++ StackComposed_provider.py
@@ -18,9 +18,14 @@
  *                                                                         *
  ***************************************************************************/
 """
+import os
 
+from qgis.PyQt.QtGui import QIcon
 from qgis.core import QgsProcessingProvider
 from StackComposed.StackComposed_algorithm import StackComposedAlgorithm
+
+# plugin path
+plugin_folder = os.path.dirname(os.path.dirname(__file__))
 
 
 class StackComposedProvider(QgsProcessingProvider):
@@ -68,7 +73,7 @@ class StackComposedProvider(QgsProcessingProvider):
         Should return a QIcon which is used for your provider inside
         the Processing toolbox.
         """
-        return QgsProcessingProvider.icon(self)
+        return QIcon(os.path.join(plugin_folder, 'icons', 'stack_composed.svg'))
 
     def longName(self):
         """
