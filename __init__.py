72e5b7ecc6113854f907e47e633eb30053255789
XavierCLL <xavier.corredor.llano@gmail.com>
2021-01-21 19:06:59 -0800

    Added install script for Dask library

--- __init__.py
+++ __init__.py
@@ -19,6 +19,35 @@
  ***************************************************************************/
 """
 
+from qgis.PyQt.QtCore import QCoreApplication
+from qgis.PyQt.QtWidgets import QMessageBox
+
+from StackComposed.utils.extra_deps import load_install_extra_deps, WaitDialog
+
+
+def pre_init_plugin(iface):
+    app = QCoreApplication.instance()
+    parent = iface.mainWindow()
+    dialog = None
+    log = ''
+    try:
+        for msg_type, msg_val in load_install_extra_deps():
+            app.processEvents()
+            if msg_type == 'log':
+                log += msg_val
+            elif msg_type == 'needs_install':
+                dialog = WaitDialog(parent, 'Stack Composed - installing dependencies')
+            elif msg_type == 'install_done':
+                dialog.accept()
+    except Exception as e:
+        if dialog:
+            dialog.accept()
+        QMessageBox.critical(parent, 'Stack Composed - installing dependencies',
+                             'An error occurred during the installation of Python packages. ' +
+                             'Click on "Stack Trace" in the QGIS message bar for details.')
+        raise RuntimeError('\nStack Composed: Error installing Python packages. Read install instruction: '
+                           'https://github.com/SMByC/StackComposed-Qgis-processing\nLog:\n' + log) from e
+
 
 # noinspection PyPep8Naming
 def classFactory(iface):  # pylint: disable=invalid-name
@@ -27,6 +56,9 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :param iface: A QGIS interface instance.
     :type iface: QgsInterface
     """
+    # load/install extra python dependencies
+    pre_init_plugin(iface)
+
     #
     from StackComposed.StackComposed_plugin import StackComposedPlugin
     return StackComposedPlugin()
